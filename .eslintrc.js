6a7c41c08d9b578ea77dd6c8813bef9a33a4f435
RThong <403216075@qq.com>
2022-03-30 16:23:32 +0800

    feat: 添加eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    'alloy',
+    'alloy/react',
+    'alloy/typescript',
+    "plugin:react-hooks/recommended"
+  ],
+  plugins: ["simple-import-sort", "import"],
+  env: {
+    // 你的环境变量（包含多个预定义的全局变量）
+    //
+    // browser: true,
+    // node: true,
+    // mocha: true,
+    // jest: true,
+    // jquery: true
+  },
+  globals: {
+    // 你的全局变量（设置为 false 表示它不允许被重新赋值）
+    //
+    // myGlobal: false
+  },
+  rules: {
+    // 自定义你的规则
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import type { FC } from "react";
+import { useState } from "react";
+
+import logo from "./logo.svg";
+
+const App: FC = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -27,7 +30,6 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +41,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
